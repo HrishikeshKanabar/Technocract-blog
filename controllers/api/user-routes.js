31d0ff20e4b4185e99591f16e564d22f716af856
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2021-11-28 13:14:59 -0500

    User route with login and delete

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,105 @@
+const router = require("express").Router();
+
+const { User, Post, Comment } = require("../../models");
+
+// /api/users
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  }).then((UserData) => {
+    res.json(UserData);
+  });
+});
+
+//  /api/users  by id
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "post_content", "created_at"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
+          model: Post,
+          attributes: ["title"],
+        },
+      },
+    ],
+  }).then((UserData) => {
+    res.json(UserData);
+  });
+});
+
+//  /api/users
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  }).then((UserData) => {
+    res.json(UserData);
+  });
+});
+
+// Login user
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((UserData) => {
+    if (!UserData) {
+      res.json({ message: "User does not exists" });
+    }
+
+    const goodPass = UserData.checkPassword(req.body.password);
+
+    if (!goodPass) {
+      res.status(400).json({ message: "Incorrect Password!" });
+      return;
+    }
+
+    res.json({ user: UserData, message: "Successfully logged in!" });
+  });
+});
+
+// log-out
+router.post("/logout", (req, res) => {});
+
+// /api/users/ by id
+router.put("/:id", (req, res) => {
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id,
+    },
+  }).then((dbUserData) => {
+    res.json(dbUserData);
+  });
+});
+
+// /api/users/ by id
+router.delete("/:id", (req, res) => {
+  Comment.destroy({
+    where: {
+      user_id: req.params.id,
+    },
+  }).then(() => {
+    User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    }).then((UserData) => {
+      res.json(UserData);
+    });
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+// import sequelize connection
+const seq = require('./config/connection');
 const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Static directory
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: true });
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });
+  console.log('App listening on port ${PORT}!');
+});
