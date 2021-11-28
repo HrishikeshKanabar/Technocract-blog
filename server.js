4e75d2fd6bda8423a490a78a2d9739d20afa0358
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2021-11-28 11:48:07 -0500

    models and config folder rename

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const sequelize = require("./connection/conn");
+const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,10 +11,10 @@ app.use(express.json());
 
 // Static directory
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
   });
